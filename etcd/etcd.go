b4b62205d469c1f31a5ff8c89fc051186bb6f6ad
zhongqingtian <zhongqingtian@wps.cn>
2020-11-13 18:28:26 +0800

    添加etcd

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -7,43 +7,57 @@ import (
 	"time"
 )
 
-func CreateEtcdClient() {
+type EtcdClient struct {
+	client *clientv3.Client
+}
+
+func CreateClient() *EtcdClient {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:2379"},
+		Endpoints:   []string{"localhost:2379"},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
-		// handle error!
 		fmt.Printf("connect to etcd failed, err:%v\n", err)
-		return
+		return nil
 	}
 	fmt.Println("connect to etcd success")
-	defer cli.Close()
-	// put
+	// defer cli.Close()
+	return &EtcdClient{client: cli}
+}
+
+// put
+func (cli *EtcdClient) Put(key, value string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	_, err = cli.Put(ctx, "q1mi", "dsb")
+	_, err := cli.client.Put(ctx, key, value)
 	cancel()
 	if err != nil {
 		fmt.Printf("put to etcd failed, err:%v\n", err)
-		return
+		return err
 	}
+	return nil
+}
+
+// get
+
+func (cli *EtcdClient) Get(key string) (*clientv3.GetResponse, error) {
 	// get
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	resp, err := cli.Get(ctx, "q1mi")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	resp, err := cli.client.Get(ctx, key)
 	cancel()
 	if err != nil {
 		fmt.Printf("get from etcd failed, err:%v\n", err)
-		return
+		return nil, nil
 	}
 	for _, ev := range resp.Kvs {
 		fmt.Printf("%s:%s\n", ev.Key, ev.Value)
 	}
+	return resp, nil
 }
 
 // watch demo
 func Watch() {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"127.0.0.1:2379"},
+		Endpoints:   []string{"localhost:2379"},
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
--- etcd/etcd_test.go
+++ etcd/etcd_test.go
@@ -1,7 +1,23 @@
 package etcd
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
+)
 
 func TestCreateEtcdClient(t *testing.T) {
-	CreateEtcdClient()
+	cli := CreateClient()
+	/*cli.Put("abc", "123")
+	cli.Put("abc", "1234")
+	cli.Get("abc")*/
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	rsp, err := cli.client.MemberList(ctx)
+	t.Log(err)
+	t.Log(rsp)
+}
+
+func TestWatch(t *testing.T) {
+	Watch()
 }
